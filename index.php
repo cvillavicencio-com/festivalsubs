ecb7048d6f7d70bbd243fa8be3c7de47a90cfac6
Camilo Villavicencio <laboral@cvillavicencio.com>
2022-09-20 13:49:25 -0300

    no corta líneas.

--- index.php
+++ index.php
@@ -1,28 +1,54 @@
+#/bin/sh
 clear;
 <?php
-
-
-
 $texto0= file_get_contents('leer');
-
-
-
 $texto1=explode('.',$texto0);
-
-
+$largodelinea=60;
 $res ='';
+$res .= "# largo de linea que se muestra: $largodelinea ".PHP_EOL;
+
 foreach ($texto1 as &$l){
     if ($l!=""){
         $l= str_replace('\' ', '\'',$l);
         $l= str_replace('  ',' ',$l);
+        $l= str_replace(PHP_EOL.PHP_EOL,PHP_EOL,$l);
+        $l= $l.'.';
+        $cora=strlen($l);
+        $res .= "# largo de oracion: $cora".PHP_EOL;                   
+        $colita = ($cora % $largodelinea != 0) ? intval($cora/$largodelinea): false;
+        if ($colita){
+            $agrega=(($colita+1)*$largodelinea);
+            $res .= "# se agrega hasta: $agrega".PHP_EOL;           
+            $l = str_pad($l,$agrega);
+            $res .= "# largo actual de oracion: ".strlen($l).PHP_EOL;           
 
-        $p=str_split($l,50); 
-
-        foreach($p as &$t){
-            //            $t=utf8_encode($t);
-            $res .= "echo '$t'".PHP_EOL;
         }
-        $res .= "echo '$l' | iconv -f utf-8 -t iso-8859-1|festival --tts ; clear".PHP_EOL;        
+        $p=explode(' ',$l);
+        $r='';
+        $clinea=1;
+        $linea='';
+        $va=0;
+        for ($i=0; $i <= (count($p)); $i++){
+            $estalinea=strlen($linea);
+            if (($estalinea+strlen($p[($i+1)])) >= $largodelinea){
+                $res .= PHP_EOL.PHP_EOL."# linea: $clinea de $colita. ".PHP_EOL."# caracteres en esta linea: $estalinea".PHP_EOL;
+                $res .= "echo '$linea'".PHP_EOL;
+                $clinea++;
+                $va +=$clinea;
+                $i--;
+                $linea='';
+                $finlinea=true;
+            } else {
+                $linea .= "{$p[$i]} ";
+            }
+
+            if ($colita > $clinea && $finlinea){
+                $res .= PHP_EOL."# va: $va";
+                $finlinea=false;
+            }
+            
+        }
+        $res .= PHP_EOL.PHP_EOL."echo '$l' | iconv -f utf-8 -t iso-8859-1|festival --tts ; clear".PHP_EOL;        
     }
 }
 echo $res;
